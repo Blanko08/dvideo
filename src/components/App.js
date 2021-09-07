00ec4994c87b4f6ece5718ff8e631fb4885392b7
Santiago <sbherrera08@gmail.com>
2021-09-07 11:45:22 +0200

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -17,76 +17,136 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+    } else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('No ethereum browser is installed. Try it installing MetaMask.');
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
-    //Load accounts
-    //Add first account the the state
+    const web3 = window.web3;
 
-    //Get network ID
-    //Get network data
-    //Check if net data exists, then
-      //Assign dvideo contract to a variable
-      //Add dvideo to the state
+    // Cargar cuenta
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
 
-      //Check videoAmounts
-      //Add videAmounts to the state
+    // Recoger contrato
+    const networkId = await web3.eth.net.getId();
+    const netWorkData = DVideo.networks[networkId];
+    if(netWorkData) {
+      // Asignar contracto
+      const dVideo = new web3.eth.Contract(DVideo.abi, netWorkData.address);
+      this.setState({ dVideo });
 
-      //Iterate throught videos and add them to the state (by newest)
+      // Comprobar cantidad de videos
+      const videosCount = await dVideo.methods.videoCount().call()
+      this.setState({ videosCount });
 
+      // Cargar videos ordenandolos de nuevo a viejo
+      for(var i = videosCount; i>=1; i--) {
+        const video = await dVideo.methods.videos(i).call();
+        this.setState({
+          videos: [...this.state.videos, video]
+        });
+      }
 
-      //Set latest video and it's title to view as default 
-      //Set loading state to false
+      // Selecciona el último video para ver por defecto.
+      const latest = await dVideo.methods.videos(videosCount).call();
+      this.setState({
+        currentHash: latest.hash,
+        currentTitle: latest.title
+      });
 
-      //If network data doesn't exisits, log error
+      this.setState({ loading: false });
+    }else {
+      window.alert('DVideo contract not deployed to detected network.');
+    }
   }
 
   //Get video
   captureFile = event => {
-
+    event.preventDefault();
+		
+		const file = event.target.files[0];
+		const reader = new window.FileReader();
+		
+		reader.readAsArrayBuffer(file);
+		reader.onloadend = () => {
+			this.setState({ buffer: Buffer(reader.result) });
+			console.log('buffer', this.state.buffer);
+		}
   }
 
   //Upload video
   uploadVideo = title => {
-
+    console.log('Submitting video to IPFS...');
+		
+		// Add video to the IPFS
+		ipfs.add(this.state.buffer, (error, result) => {
+			console.log('IPFS result', result);
+			if(error) {
+				console.log(error);
+				return;
+			}
+			
+			this.setState({ loading: true });
+			
+			// Call smart contract uploadFile function 
+			this.state.dVideo.methods.uploadVideo(result[0].hash, title).send({ from: this.state.account })
+			.on('transactionHash', (hash) => {
+				this.setState({ loading: false });
+				window.location.reload();
+			}).on('error', (e) => {
+				window.alert('Error');
+				this.setState({ loading: false });
+			});
+    });
   }
 
   //Change Video
   changeVideo = (hash, title) => {
-
+    this.setState({ 
+      'currentHash': hash, 
+      'currentTitle': title
+    });
   }
 
   constructor(props) {
     super(props)
     this.state = {
+      buffer: null,
+      account: '',
+      dvideo: null,
+      videos: [],
+      currentHash: null,
+      currentTitle: null,
       loading: false
-      //set states
     }
-
-    //Bind functions
+    
+    this.captureFile = this.captureFile.bind(this);
+    this.uploadVideo = this.uploadVideo.bind(this);
+    this.changeVideo = this.changeVideo.bind(this);
   }
 
   render() {
     return (
       <div>
         <Navbar 
-          //Account
+          account={ this.state.account }
         />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-              //states&functions
+              videos={ this.state.videos }
+              captureFile={ this.captureFile }
+              uploadVideo={ this.uploadVideo }
+              currentHash={ this.state.currentHash }
+              currentTitle={ this.state.currentTitle }
+              changeVideo={ this.changeVideo }
             />
         }
       </div>
--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,45 @@
 pragma solidity ^0.5.0;
 
 contract DVideo {
+  // Variables
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  mapping(uint => Video) public videos;
 
-  //Create Struct
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address author;
+  }
 
 
-  //Create Event
+  // Eventos
+  event VideoUploaded(uint id,
+    string hash,
+    string title,
+    address author
+  );
 
-
+  // Constructor
   constructor() public {
   }
 
+  // Funciones
+  /**
+   * @notice Función que permite subir videos.
+   * @param _videoHash Hash del video.
+   * @param _title Título del video.
+   */
   function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
+    require(bytes(_videoHash).length > 0, 'Debes insertar el hash del video.');
+    require(bytes(_title).length > 0, 'Debes insertar un título para el video.');
+    require(msg.sender != address(0), 'La dirección de la persona que quiere añadir el video debe existir.');
 
-    // Make sure video title exists
+    videoCount++;
 
-    // Make sure uploader address exists
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
 
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
   }
 }
